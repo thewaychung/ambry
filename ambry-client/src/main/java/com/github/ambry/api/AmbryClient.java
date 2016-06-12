06e6183ce9306879ae27ca5f773f294d6f569abe
Zhong.Zewei <thewaychung@gmail.com>
2016-06-12 08:34:15 +0800

    Update ambry-client api

--- ambry-client/src/main/java/com/github/ambry/api/AmbryClient.java
+++ ambry-client/src/main/java/com/github/ambry/api/AmbryClient.java
@@ -3,6 +3,9 @@ package com.github.ambry.api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.InputStream;
+
 /**
  * This is a client for Java application to invoke Ambry REST api.
  *
@@ -13,7 +16,32 @@ public class AmbryClient implements IAmbryClient{
     private final Logger logger = LoggerFactory.getLogger(AmbryClient.class);
 
     @Override
-    public String getFileInfo(String ambryId) {
+    public int healthCheck() {
+        return 0;
+    }
+
+    @Override
+    public String getFileProperty(String ambryId) {
+        return null;
+    }
+
+    @Override
+    public String postFile(String filePath) {
+        return null;
+    }
+
+    @Override
+    public String postFile(InputStream inputStream) {
+        return null;
+    }
+
+    @Override
+    public String postFile(File file) {
+        return null;
+    }
+
+    @Override
+    public String deleteFile(String ambryId) {
         return null;
     }
 }
--- ambry-client/src/main/java/com/github/ambry/api/IAmbryClient.java
+++ ambry-client/src/main/java/com/github/ambry/api/IAmbryClient.java
@@ -1,9 +1,54 @@
 package com.github.ambry.api;
 
+import java.io.File;
+import java.io.InputStream;
+
 /**
+ * This is a client for Java application to invoke Ambry REST api.
+ *
  * @author Zhong.Zewei Create on 2016.06.08.
  */
 public interface IAmbryClient {
 
-    String getFileInfo(String ambryId);
+    /**
+     * Check the status of the frontend
+     * @return the status code
+     */
+    int healthCheck();
+
+    /**
+     * Get the file properties represented by the ambryId
+     * @param ambryId the file's ambryId
+     * @return file's info of the given resource id
+     */
+    String getFileProperty(String ambryId);
+
+    /**
+     * Upload a file with the file's path.
+     * @param filePath file's path at local
+     * @return information with save status and the resource id, i.e. ambryId, response from Amby
+     */
+    String postFile(String filePath);
+
+    /**
+     * Upload a file with the given input stream
+     * @param inputStream file's input stream
+     * @return save status and the resource id response from Ambry
+     */
+    String postFile(InputStream inputStream);
+
+    /**
+     * Upload a file
+     * @param file the file which will be uploaded
+     * @return save status and the resource id response from Ambry
+     */
+    String postFile(File file);
+
+    /**
+     * Delete a file with a given resource Id
+     * @param ambryId resource id of the target file
+     * @return remove status
+     */
+    String deleteFile(String ambryId);
+
 }
